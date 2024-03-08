2542a1fbcf8c1ee33687f66930e5b9b714788b8e
Haseeb Ahmed Chaudhary <83727520+hch-web@users.noreply.github.com>
2024-03-08 14:21:37 +0500

    Unnecessary console statement removed

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,11 +2,6 @@ import { StatusCodes } from 'http-status-codes';
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
-  console.log(
-    'ERROR HANDLER',
-    Object.values(err.errors).map(val => val.message),
-  );
-
   if (err.name === 'ValidationError') {
     const messages = Object.values(err.errors).map(val => val.message);
 
